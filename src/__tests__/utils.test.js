1cd71a5a523091efb7e3e787f5077b9c5477c9ec
Joseph Andrew Phillips <josephandrewphillips@gmail.com>
2025-03-04 22:39:05 -0500

    Meets all deliverables

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,28 @@
-// Your tests here
+import { isPalindrome } from "../utils";
+
+describe('isPalindrome', () => {
+
+    it('returns true if the word is a palindrome', () => {
+        const word = 'racecar'
+        const bool = isPalindrome(word)
+        expect(bool).toBe(true)
+    })
+
+    it('returns false is the word is NOT a palindrome', () => {
+        const word = 'tree'
+        const bool = isPalindrome(word)
+        expect(bool).toBe(false)
+    })
+
+    it('returns true if the word is a palindrome with uppercase and lowercase letters', () => {
+        const word = 'rAceCar'
+        const bool = isPalindrome(word)
+        expect(bool).toBe(true)
+    })
+
+    it('returns false if the string is empty', () => {
+        const word = ''
+        const bool = isPalindrome(word)
+        expect(bool).toBe(false)
+    })
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,11 @@
-// Your code here
+export function isPalindrome(word) {
+    if (word.length > 0) {
+        let backwards = []
+        for (const char in word) {
+            backwards.unshift(word[char])
+        }
+        return backwards.join('').toLowerCase() === word.toLowerCase()
+    } else {
+        return false
+    }
+}
